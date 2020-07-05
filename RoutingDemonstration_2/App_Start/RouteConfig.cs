7f14ecd537084932eae543b2c3e57a2741476333
Utpal Maiti <Utpalmaiti12@gmail.com>
2020-07-05 07:58:23 +0530

    Add project files.

---
+++ RoutingDemonstration_2/App_Start/RouteConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace RoutingDemonstration_2
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapRoute(
+                name: "DemoRoute",
+                url: "Pragim/{controller}/{action}/{id}",
+                defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Admin",
+                url: "Admin/{controller}/{action}/{id}",
+                defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ RoutingDemonstration_2/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoutingDemonstration_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //return RedirectToAction("EditEmployee",new { controller = "Home" ,
+            //    id =123, Name="Abhishek"});
+
+            //return RedirectToRoute("Default", new
+            //{
+            //    controller = "Home",
+            //    action = "EditEmployee",
+            //    id = 123,
+            //    Name = "Abhishek"
+            //});
+
+            return View();
+            //return RedirectToActionPermanent
+        }
+
+        public string EditEmployee()
+        {
+            return "Welcome to EditEmployee Method";
+        }
+
+        public string DeleteEmployee()
+        {
+            return "Welcome to DeleteEmployee Method";
+        }
+    }
+}
